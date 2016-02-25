cb2761b2aad89a49ad15ce9e22b83075099e63bd
Josh Goldberg <josh@fullscreenmario.com>
2016-02-25 10:40:44 -0800

    Split HttpRestClient out of PoC; added PUT

--- Client/Client/App.xaml.cs
+++ Client/Client/App.xaml.cs
@@ -38,7 +38,7 @@
             }
             set
             {
-                ApplicationData.Current.RoamingSettings.Values[StorageFolderPathKey] = value;
+                ApplicationData.Current.RoamingSettings.Values[StorageFolderPathKey] = value.Trim(' ', '/', '\\');
             }
         }
 
