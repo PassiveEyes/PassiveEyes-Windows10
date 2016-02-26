007b062ee8e046888cfde4569a8701496381f58b
Josh Goldberg <josh@fullscreenmario.com>
2016-02-25 22:19:32 -0800

    Added skeleton prototype for RecorderPage
    A text block is added to a grid view for each camera. Behind the scenes,
    the same `CameraReceiver` logic is fired.

---
+++ Client/Client/Models/Camera.cs
@@ -0,0 +1,76 @@
+﻿namespace Client.Models
+{
+    using Media;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Windows.Devices.Enumeration;
+    using Windows.Media.Capture;
+    using Windows.UI.Xaml.Controls;
+
+    /// <summary>
+    /// A representation of a webcam or camera.
+    /// </summary>
+    public class Camera
+    {
+        /// <summary>
+        /// The name of the camera.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Handler for the camera input.
+        /// </summary>
+        public CameraReceiver Receiver { get; private set; }
+
+        /// <summary>
+        /// God only knows what the hell I'm supposed to do with this.
+        /// </summary>
+        public CaptureElement CaptureElement { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Camera"/> class.
+        /// </summary>
+        /// <param name="name">The name of the camera.</param>
+        /// <param name="cameraDevice">Information on the streaming camera device.</param>
+        /// <param name="uploadHandler">An external handler for uploading a photo.</param>
+        public Camera(string name, DeviceInformation cameraDevice)
+        {
+            this.Name = name;
+            this.Receiver = new CameraReceiver(cameraDevice, this.HandleUpload);
+        }
+
+        /// <summary>
+        /// Initializes the receiver and does some CaptureElement stuff.
+        /// </summary>
+        public async Task Initialize()
+        {
+            await this.Receiver.Initialize();
+
+            this.CaptureElement = new CaptureElement();
+            this.CaptureElement.Source = this.Receiver.MediaCapture;
+        }
+
+        /// <summary>
+        /// Handles the receiver signaling for an upload.
+        /// </summary>
+        /// <param name="mediaCapture">The receiver's media.</param>
+        private Task HandleUpload(MediaCapture mediaCapture)
+        {
+            System.Diagnostics.Debugger.Break();
+            return null;
+        }
+
+        /// <summary>
+        /// Converts available cameras into <see cref="Camera"/> instances.
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<IEnumerable<Camera>> CollectCameras()
+        {
+            return 
+                (await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture))
+                    .Select((device, i) => new Camera($"Camera {i}", device));
+        }
+    }
+}
---
+++ Client/Client/Models/ReorderPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace Client.Models
+{
+    using System.Collections.ObjectModel;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// A code-behind for the <see cref="ReorderPage"/>.
+    /// </summary>
+    public class ReorderPageViewModel
+    {
+        /// <summary>
+        /// Available cameras.
+        /// </summary>
+        public ObservableCollection<Camera> Cameras { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReorderPageViewModel"/> class.
+        /// </summary>
+        public ReorderPageViewModel()
+        {
+            this.Cameras = new ObservableCollection<Camera>();
+        }
+
+        /// <summary>
+        /// Collects available cameras and initializes them.
+        /// </summary>
+        /// <returns></returns>
+        public async Task Initialize()
+        {
+            foreach (var camera in await Camera.CollectCameras())
+            {
+                this.Cameras.Add(camera);
+            }
+
+            foreach (var camera in this.Cameras)
+            {
+                await camera.Initialize();
+            }
+        }
+    }
+}
