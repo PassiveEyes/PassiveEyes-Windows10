b4bb17fa18277d1d99d9fb4c0ecabd7e83888ba9
Josh Goldberg <josh@fullscreenmario.com>
2016-02-25 23:08:21 -0800

    Added working RecorderPage with preview video
    Data bound video context via a custom user control.

---
+++ Client/Client/Controls/RecordingCamera.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace Client.Controls
+{
+    using Models;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.InteropServices.WindowsRuntime;
+    using Windows.Foundation;
+    using Windows.Foundation.Collections;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Controls.Primitives;
+    using Windows.UI.Xaml.Data;
+    using Windows.UI.Xaml.Input;
+    using Windows.UI.Xaml.Media;
+    using Windows.UI.Xaml.Navigation;
+
+    /// <summary>
+    /// Code-behind 
+    /// </summary>
+    public sealed partial class RecordingCamera : UserControl
+    {
+        public Camera Camera { get; set; }
+
+        public RecordingCamera()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            await this.Camera.Receiver.Initialize();
+            this.PreviewControl.Source = this.Camera.Receiver.MediaCapture;
+            await this.Camera.Receiver.MediaCapture.StartPreviewAsync();
+        }
+    }
+}
--- Client/Client/Models/ReorderPageViewModel.cs
+++ Client/Client/Models/ReorderPageViewModel.cs
@@ -31,11 +31,6 @@
             {
                 this.Cameras.Add(camera);
             }
-
-            foreach (var camera in this.Cameras)
-            {
-                await camera.Initialize();
-            }
         }
     }
 }
